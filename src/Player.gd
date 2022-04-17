65c29adf6cb77aebd789ad71276ad287ecdd6666
Zxmon <68187602+Zxmon-code@users.noreply.github.com>
2022-04-17 12:25:51 +0200

    Added dash mechanic

--- src/Player.gd
+++ src/Player.gd
@@ -4,15 +4,20 @@ extends KinematicBody2D
 const gravity = 30
 const speed = 400
 const jumpforce = -600
-var canjump = 0
-var facing = 0
-
+var canjump = false
 var velocity = Vector2()
 export var jumpspeed = 700
 var jump = 0
 var input
 var horizontal_speed = null
 const ACCELERATION = 3000
+const DASHSPEED = 1600
+var candash = false
+var facing_direction = null
+var dashing = false
+onready var timer = get_node("Timer")
+const DASHCOOLDOWN = .5
+var dashtimertimeout = true
 
 
 
@@ -27,45 +32,83 @@ const ACCELERATION = 3000
 
 #Better Input(using action strength
 func get_horizontal_input(delta):
-	horizontal_speed = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	if horizontal_speed != 0.0:
-		velocity.x += ACCELERATION * horizontal_speed * delta
-		if horizontal_speed>0.01 and velocity.x > speed*horizontal_speed:
-			velocity.x = speed*horizontal_speed
-		if horizontal_speed < -0.01 and velocity.x < speed*horizontal_speed:
-			velocity.x = speed*horizontal_speed
-	else:
-		velocity.x = velocity.move_toward(Vector2.ZERO, ACCELERATION*2*delta).x
+	if !dashing:
+		horizontal_speed = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+		if horizontal_speed != 0.0:
+			velocity.x += ACCELERATION * horizontal_speed * delta
+			if horizontal_speed>0.01 and velocity.x > speed*horizontal_speed:
+				velocity.x = speed*horizontal_speed
+			if horizontal_speed < -0.01 and velocity.x < speed*horizontal_speed:
+				velocity.x = speed*horizontal_speed
+		else:
+			velocity.x = velocity.move_toward(Vector2.ZERO, ACCELERATION*2*delta).x
 	
 
 func jumping():
-	if Input.is_action_just_pressed("move_up") and Input.is_action_pressed("move_right") and canjump==1 and is_on_wall():
-		velocity.x = -1.5*speed
-		jump_without_player_input()
-		#canjump = 0 #this does not work
-	elif Input.is_action_just_pressed("move_up") and Input.is_action_pressed("move_left") and canjump==1 and is_on_wall():
-		velocity.x = 1.5*speed
-		jump_without_player_input()
-		#canjump = 0 #this does not work
-	elif Input.is_action_pressed("move_up") and canjump == 1: 
-		velocity.y = jumpforce
-		canjump = 0
-	if is_on_wall() and velocity.x != 0:
-		velocity.y = 0
+	if !dashing:
+		if Input.is_action_just_pressed("move_up") and Input.is_action_pressed("move_right") and canjump and is_on_wall():
+			velocity.x = -1.5*speed
+			yield(get_tree().create_timer(.02), "timeout")
+			jump_without_player_input()
+			#canjump = 0 #this does not work
+		elif Input.is_action_just_pressed("move_up") and Input.is_action_pressed("move_left") and canjump and is_on_wall():
+			velocity.x = 1.5*speed
+			yield(get_tree().create_timer(.02), "timeout")
+			jump_without_player_input()
+			#canjump = 0 #this does not work
+		elif Input.is_action_pressed("move_up") and canjump: 
+			velocity.y = jumpforce
+			canjump = false
+		if is_on_wall() and velocity.x != 0:
+			velocity.y = 0
 
 func jump_without_player_input():
 	velocity.y = jumpforce
+	canjump = false
+	
+func dash():
+	if Input.is_action_just_pressed("dash") and candash and dashtimertimeout:
+		dashtimertimeout = false
+		velocity.y = 0
+		velocity.x = facing_direction*DASHSPEED
+		candash=false
+		dashing=true
+		yield(get_tree().create_timer(.2), "timeout")
+		dashing=false
+		timer.start(DASHCOOLDOWN)
+		
 
 func _physics_process(delta):
 	get_horizontal_input(delta)
 #	get_player_input()
 	if is_on_floor():
-		canjump = 1
+		canjump = true
+		candash = true
 	elif is_on_wall() and velocity.x != 0:
-		canjump = 1
+		canjump = true
 	velocity.y = velocity.y + gravity
 	if is_on_wall() and velocity.x != 0 and velocity.y == 0:
 		velocity.y = 0
 	jumping()
-	print(velocity.x)
+#	print(canjump)
+	if velocity.x > 0:
+		facing_direction = 1
+	elif velocity.x < 0:
+		facing_direction = -1
+	
+	dash()
+	var vel0 = 0
+	if dashing:
+		if vel0 < 6:
+			velocity.y = 0
+			vel0 += 1
+		elif vel0 == 6:
+			vel0 = 0
+		
+	
 	velocity = move_and_slide(velocity, Vector2.UP)
+	
+
+
+func _on_Timer_timeout():
+	dashtimertimeout = true
